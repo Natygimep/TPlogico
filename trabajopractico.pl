686b6ff7a64a232ffca8ff213727b9de068350e2
Naty <natygimenezpizzio@gmail.com>
2017-05-17 20:33:17 -0300

    Corregimos los errores en los predicados.

--- trabajopractico.pl
+++ trabajopractico.pl
@@ -1,8 +1,8 @@
-propiedad(Tinsmith Circle 17741, 700, 3).
-propiedad(Av. Moreno 7082, 2000, 7).
-propiedad(Av. Siempre Viva 742, 1000, 4).
-propiedad(Calle Falsa 123, 200, 3).
-tieneJardin(Tinsmith Circle17741).
-tieneJardin(Av. Moreno7082).
-tieneJardin(Av. Siempre Viva 742).
-tienePiscina(Av. Moreno 7082, 30).
+propiedad(tinsmithCircle17741, 700, 3).
+propiedad(avMoreno7082, 2000, 7).
+propiedad(avSiempreViva742, 1000, 4).
+propiedad(calleFalsa123, 200, 3).
+tieneJardin(tinsmithCircle17741).
+tieneJardin(avMoreno7082).
+tieneJardin(avSiempreViva742).
+tienePiscina(avMoreno7082, 30).
