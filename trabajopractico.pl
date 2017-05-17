3bd2f37b5f198759da50bbb55c377f213eed7854
Naty <natygimenezpizzio@gmail.com>
2017-05-17 08:21:52 -0300

    Creamos los jardines.

--- trabajopractico.pl
+++ trabajopractico.pl
@@ -2,3 +2,6 @@ propiedad(Tinsmith Circle 17741, 700, 3).
 propiedad(Av. Moreno 7082, 2000, 7).
 propiedad(Av. Siempre Viva 742, 1000, 4).
 propiedad(Calle Falsa 123, 200, 3).
+tieneJardin(Tinsmith Circle17741).
+tieneJardin(Av. Moreno7082).
+tieneJardin(Av. Siempre Viva 742).
