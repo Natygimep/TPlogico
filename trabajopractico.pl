9be0507e44e5e2fd282187b963ba0ca31b458379
Naty <natygimenezpizzio@gmail.com>
2017-05-25 22:02:24 -0300

    Tratando de dominar Kraken.

--- trabajopractico.pl
+++ trabajopractico.pl
@@ -1,11 +1,50 @@
-propiedad(tinsmithCircle17741, 700, 3).
 propiedad(avMoreno7082, 2000, 7).
 propiedad(avSiempreViva742, 1000, 4).
 propiedad(calleFalsa123, 200, 3).
+
 tieneJardin(tinsmithCircle17741).
 tieneJardin(avMoreno7082).
 tieneJardin(avSiempreViva742).
 tienePiscina(avMoreno7082, 30).
-tieneMismosAmbientes(UnaPropiedad, OtraPropiedad) :- propiedad(UnaPropiedad, _, Ambientes),
-                              propiedad(OtraPropiedad, _, Ambientes),
-                              UnaPropiedad \= OtraPropiedad.
+
+cliente(carlos).
+cliente(ana).
+cliente(maria).
+cliente(pedro).
+cliente(chamaleon).
+
+tiene(tinsmithCircle17741, jardin).
+tiene(avMoreno7082, jardin).
+tiene(avSiempreViva742, jardin).
+tiene(avMoreno7082, piscina(30)).
+
+tieneMismosAmbientes(UnaPropiedad, OtraPropiedad) :-
+       propiedad(UnaPropiedad, _, Ambientes),
+       propiedad(OtraPropiedad, _, Ambientes),
+       UnaPropiedad \= OtraPropiedad.
+
+busca(carlos, jardin).
+busca(carlos, ambientes(3)).
+busca(ana, piscina(100)).
+busca(maria, ambientes(2)).
+busca(maria, piscina(15)).
+busca(pedro, UnRequerimiento) :-
+       busca(maria, UnRequerimiento).
+busca(chamaleon, UnRequerimiento) :-
+       cliente(UnCliente),
+       busca(UnCliente, UnRequerimiento),
+       UnCliente \= chamaleon.
+
+requiere(UnCliente, UnRequerimiento) :-
+      cliente(UnCliente),
+      busca(UnCliente, UnRequerimiento).
+
+
+% Consultas
+% 1 Si existe alguna propiedad con una piscina de 30 metros. Y si es que existe, cuál es.
+% ?- tienePiscina(X, 30).
+% X = avMoreno708
+% 2 Si existe alguna propiedad que tenga la misma cantidad de ambientes que otra. Y si existen, cuáles son.
+% ?- tieneMismosAmbientes(X,Y).
+% X = tinsmithCircle17741,
+% Y = calleFalsa123 .
