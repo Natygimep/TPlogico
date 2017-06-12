dc9d64e88f7ac91d2a51acf0e4657695b2bb0c71
Unknown <brau96_2009@yahoo.com.ar>
2017-06-11 21:15:13 -0300

    Agrego punto de propiedades top

--- trabajopractico.pl
+++ trabajopractico.pl
@@ -4,12 +4,15 @@ propiedad(avSiempreViva742, 1000).
 propiedad(calleFalsa123, 200).
 
 tiene(tinsmithCircle17741, jardin).
-tiene(avMoreno708, jardin).
-tiene(avSiempreViva742, jardin).
-tiene(avMoreno708, piscina(30)).
 tiene(tinsmithCircle17741,ambientes(3)).
+tiene(tinsmithCircle17741,instalaciones([ac,extractor,calefaccionGas])).
+tiene(avMoreno708, jardin).
+tiene(avMoreno708, piscina(30)).
 tiene(avMoreno708,ambientes(7)).
+tiene(avMoreno708,instalaciones([ac,extractor,calefaccionLoza,vidriosDobles])).
+tiene(avSiempreViva742, jardin).
 tiene(avSiempreViva742,ambientes(4)).
+tiene(avSiempreViva742,instalaciones([calefaccionGas])).
 tiene(calleFalsa123,ambientes(3)).
 
 tieneMismosAmbientes(UnaPropiedad, OtraPropiedad) :-
@@ -26,10 +29,12 @@ persona(chamaleon).
 busca(carlos, jardin).
 busca(carlos, ambientes(3)).
 busca(ana, piscina(100)).
+busca(ana, instalaciones([ac,vidriosDobles])).
 busca(maria, ambientes(2)).
 busca(maria, piscina(15)).
 busca(pedro, UnRequerimiento) :-
       busca(maria, UnRequerimiento).
+busca(pedro, instalaciones([calefaccionLoza,vidriosDobles])).
 busca(chamaleon, UnRequerimiento) :-
       persona(UnCliente),
       UnCliente \= chamaleon,
@@ -92,6 +97,24 @@ efectividad(Efectividad) :-
     length(PersonasSinRepetidos,PersonasTotales),
     length(PersonasSatisfechasSinRepetidos,PersonasSatisfechasTotales),
     Efectividad is PersonasSatisfechasTotales / PersonasTotales * 100.
+
+tieneInstalaciones(_,[]).
+tieneInstalaciones(Propiedad, [UnaInstalacion|RestoInstalaciones]) :-
+    propiedad(Propiedad,_),
+    tiene(Propiedad,instalaciones(Instalaciones)),
+    member(UnaInstalacion,Instalaciones),
+    tieneInstalaciones(Propiedad,RestoInstalaciones).
+
+esUnaPropiedadTop(Propiedad) :-
+    propiedad(Propiedad,_),
+    not(esPropiedadChica(Propiedad)),
+    tieneInstalaciones(Propiedad,[ac]).
+
+esPropiedadChica(Propiedad) :-
+    tiene(Propiedad,ambientes(1)).
+esPropiedadChica(Propiedad) :-
+    not(tiene(Propiedad,ambientes(_))).
+
 
 
 
@@ -236,3 +259,12 @@ efectividad(Efectividad) :-
 % 10. Qué efectividad tiene nuestro sistema.
 % ?- efectividad(Efectividad).
 % Efectividad = 80.0.
+
+% Al agregar lo de las instalaciones agregamos el concepto de listas, y nos facilita la modificacion de codigo
+% porque mediante recurisvidad podemos evaluar que tenga todas las instalaciones deseadas, en caso de que
+% se agreguen nuevas instalaciones, o que alguna persona desee una instalacion nueva, solo hace falta agregarla a la lista.
+
+% 11. Determinar cuáles son las propiedades top.
+% ?- esUnaPropiedadTop(Cual).
+% Cual = tinsmithCircle17741 ;
+% Cual = avMoreno708.
